cbd271435c6c6b5b7c2b0398ea305a26096df2ee
Roman Voytyuk <romavoytyuk@gmail.com>
2023-05-17 17:17:31 +0300

    domain logic is added. ShopListRepository, AddShopItemUsageCase, EditShopItemUsageCase, DeleteShopItemUsageCase, GetShopItemListUsageCase, GetShopItemUsageCase are added.

--- app/src/main/java/com/romanvoytyuk/shoplist/perfomance/MainActivity.kt
+++ app/src/main/java/com/romanvoytyuk/shoplist/perfomance/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.romanvoytyuk.shoplist
+package com.romanvoytyuk.shoplist.perfomance
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.romanvoytyuk.shoplist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
