3d1c5c2d9a5b9efe01ac1b07df6ae3a869ea3bb7
Roman Voytyuk <romavoytyuk@gmail.com>
2023-05-26 15:28:01 +0300

    swipe, longclick, click listeners created

--- app/src/main/java/com/romanvoytyuk/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/romanvoytyuk/shoplist/presentation/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.SimpleCallback
 import androidx.recyclerview.widget.RecyclerView
 import com.romanvoytyuk.shoplist.R
 
@@ -39,12 +41,45 @@ class MainActivity : AppCompatActivity() {
             )
 
         }
-        shopListAdapter.shopItemLongClick = {
-            viewModel.changeEnableState(it)
-        }
+        setupLongClickListener()
+        setupClickListener()
+        setupSwipeListener(rv)
+
+    }
+
+    private fun setupSwipeListener(rv: RecyclerView?) {
+        val itemTouchHelper = ItemTouchHelper(object : SimpleCallback(
+            0,
+            ItemTouchHelper.START or ItemTouchHelper.END
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val shopItem = shopListAdapter.shopList[position]
+                viewModel.deleteShopItem(shopItem)
+
+            }
+
+        })
+        itemTouchHelper.attachToRecyclerView(rv)
+    }
+
+    private fun setupClickListener() {
         shopListAdapter.shopItemClick = {
             Log.d("ClickListener", "Shop item name: ${it.name} price: ${it.price}")
         }
+    }
 
+    private fun setupLongClickListener() {
+        shopListAdapter.shopItemLongClick = {
+            viewModel.changeEnableState(it)
+        }
     }
 }
