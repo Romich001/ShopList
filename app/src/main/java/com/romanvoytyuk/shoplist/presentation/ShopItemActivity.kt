a95eead5de90d6be1ee4571cbf87c49175b3719a
Roman Voytyuk <romavoytyuk@gmail.com>
2023-05-29 09:35:19 +0300

    shopItemActivity created

---
+++ app/src/main/java/com/romanvoytyuk/shoplist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.romanvoytyuk.shoplist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.romanvoytyuk.shoplist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.shop_item_activity)
+    }
+}
