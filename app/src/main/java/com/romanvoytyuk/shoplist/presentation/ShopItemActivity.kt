573aa4bac8c644e6e930f94e09b66d824d26c6ea
Roman Voytyuk <romavoytyuk@gmail.com>
2023-05-30 09:46:45 +0300

    ShopItemActivity created companion object

--- app/src/main/java/com/romanvoytyuk/shoplist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/romanvoytyuk/shoplist/presentation/ShopItemActivity.kt
@@ -1,12 +1,41 @@
 package com.romanvoytyuk.shoplist.presentation
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.romanvoytyuk.shoplist.R
 
 class ShopItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.shop_item_activity)
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+
+        Log.d("ShopItemIntentExtra", mode.toString())
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val MODE_EDIT = "edit_mode"
+        private const val MODE_ADD = "add_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+
+        fun newIntentEditItem(context: Context, shopItemId: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
+            return intent
+        }
+
+
     }
 }
